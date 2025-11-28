629a891291db52745e530faeda7b3c9ab4e452dc
PHELELEKE <mkwanele535@gmail.com>
2025-11-28 11:23:17 +0200

    Refactor Program.cs for cleaner structure

--- Program.cs
+++ Program.cs
@@ -59,7 +59,8 @@ using (var scope = app.Services.CreateScope())
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Account}/{action=Login}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 app.MapRazorPages();
 
 app.Run();
+
